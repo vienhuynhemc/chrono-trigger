43c55de9815fa8bd04bafde16d4cd285a906d9b2
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-04-08 16:23:19 +0700

    Tạo project đầu tiên

--- ChronoTrigger/app/src/main/java/com/vientamthuong/chronotrigger/MainActivity.java
+++ ChronoTrigger/app/src/main/java/com/vientamthuong/chronotrigger/MainActivity.java
@@ -3,12 +3,14 @@ package com.vientamthuong.chronotrigger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
