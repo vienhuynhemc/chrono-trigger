009e0063c0131485c06beb49e6dbd7efbaae78f1
NgocTan <18130210@st.hcmuaf.edu.vn>
2021-04-08 16:38:52 +0700

    test

--- app/src/main/java/com/vientamthuong/chronotrigger/MainActivity.java
+++ app/src/main/java/com/vientamthuong/chronotrigger/MainActivity.java
@@ -3,6 +3,7 @@ package com.vientamthuong.chronotrigger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
